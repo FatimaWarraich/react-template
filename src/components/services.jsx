f89db7a5be4c7e131f6f817aa1296099461cc8aa
fatima warraich <fatimawarraich.sgd@gmail.com>
2022-06-30 23:37:26 +0500

    first commit

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,8 +5,7 @@ export const Services = (props) => {
         <div className='section-title'>
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            We would love to provide quality services
           </p>
         </div>
         <div className='row'>
