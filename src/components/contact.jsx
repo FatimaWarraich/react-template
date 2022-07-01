bfe36aac8d64fa0584000918b27835755ff837d9
fatima warraich <fatimawarraich.sgd@gmail.com>
2022-07-02 01:02:08 +0500

    2nd commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -150,9 +150,9 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
+            &copy; 2022 Design by{' '}
             <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
+              BRDIGITECH
             </a>
           </p>
         </div>
