a585de3defe1bb3aaea15ee322baefcc58696262
fatima warraich <fatimawarraich.sgd@gmail.com>
2022-07-03 01:19:17 +0500

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import { Features } from "./components/features";
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
+//import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
+import ourMission from "./components/ourMission";
 import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
@@ -31,9 +32,13 @@ const App = () => {
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery}/>
-      <Testimonials data={landingPageData.Testimonials} />
+      <ourMission/>
+     {/* 
+     <Testimonials data={landingPageData.Testimonials} > */ }
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
+      
+      
     </div>
   );
 };
